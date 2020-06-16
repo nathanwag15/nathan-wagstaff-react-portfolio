5a1b1d7d6faed84522cbd8ef691eb3ec96ebaffc
nathanwagstaff <natewagstaff21@gmail.com>
2020-06-16 13:52:26 -0600

    second commit

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import moment from "moment";
+
+import PortfolioContainer from "./portfolio/portfolio-container";
 
 export default class App extends Component {
   render() {
     return (
-      <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <h2>React Redux Router</h2>
+      <div className="app">
+        <h1>Jordan Hudgens Portfolio</h1>
+        <div>{moment().format("MMMM Do YYYY, h:mm:ss a")}</div>
+        <PortfolioContainer />
       </div>
     );
   }
