99049f0ba5e9103d6db08330c44971097c1a6077
nathanwagstaff <natewagstaff21@gmail.com>
2020-06-25 16:31:58 -0600

    login commit

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -1,67 +1,89 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
 
 export default class Login extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            email: "",
-            password: ""
-        }
+    this.state = {
+      email: "",
+      password: "",
+      errorText: ""
+    };
 
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-    handleSubmit(event) {
-        axios.post("https://api.devcamp.space/sessions",
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value,
+      errorText: ""
+    });
+  }
+
+  handleSubmit(event) {
+    axios
+      .post(
+        "https://api.devcamp.space/sessions",
         {
-            client: {
-                email: this.state.email,
-                password: this.state.password
-
-            }
+          client: {
+            email: this.state.email,
+            password: this.state.password
+          }
+        },
+        { withCredentials: true }
+      )
+      .then(response => {
+        if (response.data.status === "created") {
+            console.log("response", response);
+          this.props.handleSuccessfulAuth();
+        } else {
+          this.setState({
+            errorText: "Wrong email or password"
+          });
+          this.props.handleUnsuccessfulAuth();
         }
-        )
-        event.preventDefault(); 
-    }
-
-    handleChange(event) {
+      })
+      .catch(error => {
         this.setState({
-            [event.target.name]: event.target.value
+          errorText: "An error occurred"
         });
-    }
-    render() {
-        return (
-            <div>
-                <h1>LOGIN TO ACCESS YOUR DASHBOARD</h1>
+        this.props.handleUnsuccessfulAuth();
+      });
 
-                <form onSubmit={this.handleSubmit}>
-                    <input 
-                        type="email"
-                        name="email"
-                        placeholder="Your email"
-                        value={this.state.email}
-                        onChange={this.handleChange}
-                    />
+    event.preventDefault();
+  }
 
-                    <input 
-                        type="password"
-                        name="password"
-                        placeholder="Your password"
-                        value={this.state.password}
-                        onChange={this.handleChange}
-                    />
+  render() {
+    return (
+      <div>
+        <h1>LOGIN TO ACCESS YOUR DASHBOARD</h1>
 
-                    <div>
-                        <button type="submit">Login</button>
-                    </div>
-                </form>
+        <div>{this.state.errorText}</div>
 
-            </div>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="email"
+            name="email"
+            placeholder="Your email"
+            value={this.state.email}
+            onChange={this.handleChange}
+          />
 
+          <input
+            type="password"
+            name="password"
+            placeholder="Your password"
+            value={this.state.password}
+            onChange={this.handleChange}
+          />
 
-        )
-    }
+          <div>
+            <button type="submit">Login</button>
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
